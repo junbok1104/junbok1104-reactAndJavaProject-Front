894a616b151c38da7c7a90465ae6d5dcb413971a
junbok <bock1104@gmail.com>
2023-10-23 20:25:48 +0900

    React Route 관리 방법 - reactrouter-dom 패키지 이용

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
-import MuiUse from './MuiUse';
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+import Home from './Home';
+import Contact from './Contact';
 
 
 function App() {
 
   return (
-      <MuiUse></MuiUse>
+      <div className='App'>
+          <BrowserRouter>
+            <nav>
+              <Link to="/">Home</Link>{'  '}
+              <Link to="/contact">Contact</Link>{' '}
+            </nav>
+
+            <Routes>
+              <Route path='/' element={<Home />}></Route>
+              <Route path='contact' element={<Contact/>}></Route>
+            </Routes>
+          </BrowserRouter>
+      </div>
   );
 }
 
