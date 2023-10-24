11152dd1af430928880da189b96931e1b7121c5b
junbok <bock1104@gmail.com>
2023-10-24 21:11:57 +0900

    차량 리스트 수정 기능 추가 및 CSV 내려받기 기능 구현

--- src/Carlist.js
+++ src/Carlist.js
@@ -1,9 +1,10 @@
 
 import React, { useEffect, useState } from 'react';
 import { SERVER_URL } from './constant';
-import { DataGrid } from '@mui/x-data-grid';
-import { Snackbar } from '@mui/material';
+import { DataGrid, GridToolbarContainer, GridToolbarExport, gridClasses } from '@mui/x-data-grid';
+import { Snackbar} from '@mui/material';
 import AddCar from './AddCar';
+import EditCar from './EditCar';
 
 function Carlist() {
 
@@ -49,6 +50,13 @@ function Carlist() {
         {field: 'year', headerName: 'Year', width: 150},
         {field: 'price', headerName: 'Price', width: 150},
         {
+            field : '_links.car.href',
+            headerName: '',
+            sortable : false,
+            filterable : false,
+            renderCell : row => <EditCar data={row} updateCar={updateCar} />
+        },
+        {
             field : '_links.self.href',
             headerName: '',
             sortable : false,
@@ -76,11 +84,37 @@ function Carlist() {
         .catch(err => console.log(err))
     }
 
+    // 자동차 업데이트
+    const updateCar = (car, link) => {
+        fetch(link , 
+            {
+                method: 'PUT',
+                headers: {'Content-Type':'application/json'},
+                body: JSON.stringify(car)
+            })
+            .then(response => {
+                if(response.ok) {
+                    fetchCars();
+                }else {
+                    alert('SomeThing went wrong!');
+                }
+            })
+            .catch(err => console.error(err))
+    }
+
+    function CustomToolbar() {
+        return (
+            <GridToolbarContainer className={gridClasses.toolbarContainer}>
+                <GridToolbarExport />
+            </GridToolbarContainer>
+        )
+    }
+
     return(
         <React.Fragment>
             <AddCar addCar={addCar} />
             <div style={{height:500, width: '100%'}}>
-                <DataGrid rows={cars} columns={columns}  getRowId={row => row._links.self.href}></DataGrid>
+                <DataGrid rows={cars} columns={columns}  getRowId={row => row._links.self.href} components={{Toolbar: CustomToolbar}}></DataGrid>
                 <Snackbar open={open} autoHideDuration={2000} onClose={() => setOpen(false)} message="Car deleted" />
             </div>
         </React.Fragment>
---
+++ src/EditCar.js
@@ -0,0 +1,70 @@
+import { Dialog, DialogActions, DialogContent, DialogTitle } from "@mui/material";
+import { useState } from "react";
+
+function EditCar(props) {
+    
+    const [open, setOpen] = useState(false);
+    const [car, setCar] = useState({
+        brand: '',
+        model: '',
+        color: '',
+        year: '',
+        fuel: '',
+        price : ''
+    });
+
+    //모달 폼 열기
+    const handleClickOpen = () => {
+        setCar({
+            brand: props.data.row.brand,
+            model: props.data.row.model,
+            color: props.data.row.color,
+            year: props.data.row.year,
+            fuel: props.data.row.fuel,
+            price: props.data.row.price
+        })
+        setOpen(true);
+    };
+    
+    // 모달 폼 닫기
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const handleChange = (event) => {
+        setCar({...car, [event.target.name]:event.target.value});
+    };
+
+    //자동차를 업데이트하고 모달 폼을 닫음
+    const handleSave = () => {
+        props.updateCar(car, props.data.id);
+        handleClose();
+    }
+
+
+    return (
+        <div>
+            <button onClick={handleClickOpen}>Edit</button>
+            <Dialog open={open} onClose={handleClose}>
+                <DialogTitle>Edit Car</DialogTitle>
+                    <DialogContent>
+                        <input placeholder="Brand" name="brand" value={car.brand} onChange={handleChange}/>
+                        <br/>
+                        <input placeholder="Model" name="model" value={car.model} onChange={handleChange}/>
+                        <br/>
+                        <input placeholder="Color" name="color" value={car.color} onChange={handleChange}/>
+                        <br/>
+                        <input placeholder="Year" name="year" value={car.year} onChange={handleChange}/>
+                        <br/>
+                        <input placeholder="Price" name="price" value={car.price} onChange={handleChange}/>
+                    </DialogContent>
+                    <DialogActions>
+                        <button onClick={handleClose}>Cancel</button>
+                        <button onClick={handleSave}>Save</button>
+                    </DialogActions>
+            </Dialog>
+        </div>
+    );
+}
+
+export default EditCar;
